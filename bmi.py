33d7cddc3f08eed98161ecc9e3bddc7760e6d64c
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-07-26 18:52:02 -0400

    Create bmi.py

---
+++ bmi.py
@@ -0,0 +1,8 @@
+import math
+
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+print("your BMI is: " + str(int(weight / math.sqrt(height))))
+#Write your code below this line 👇
