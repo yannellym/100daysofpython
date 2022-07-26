34f72e7d6cb7a448d909c891a1976570c6176a02
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-07-26 19:17:18 -0400

    Create yearsuntil90.py

---
+++ yearsuntil90.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+int_age = int(age)
+
+total_days = int(365 * 90)
+total_weeks = int(52 * 90)
+total_months = int(12 * 90)
+
+
+print(f"you have {total_days - (int_age * 365)} days, {total_weeks - (int_age * 52)} weeks, {total_months - (int_age * 12)} months left.")
+#Write your code below this line 👇
