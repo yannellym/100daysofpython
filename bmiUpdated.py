2279c40e8ac50f24f324ff06558e6ae21b4a6b39
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-07-27 12:44:36 -0400

    Update bmiUpdated.py

--- bmiUpdated.py
+++ bmiUpdated.py
@@ -1,18 +1,20 @@
+import math
+
 # 🚨 Don't change the code below 👇
 height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-bmi = round(weight / ( height ** 2),2)
+bmi = math.ceil(weight / ( height ** 2))
 
 if bmi < 18.5:
-    print("You are underweight")
+    print(f"Your BMI is {bmi}, you are underweight")
 elif bmi >= 18.5 and bmi < 25:
-    print("You have a normal weight!")
+    print(f"Your BMI is {bmi}, you have a normal weight")
 elif bmi >= 25 and bmi < 30:
-    print("You are slightly overweight")
+    print(f"Your BMI is {bmi}, you are slightly overweight")
 elif bmi >= 30 and bmi < 35:
-    print("You are obese")
+    print(f"Your BMI is {bmi}, you are obese")
 else:   
-    print("You are clinically obese")
+    print(f"Your BMI is {bmi}, you are clinically obese")
