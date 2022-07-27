6189833c841bb6388bbcb8578eb5b0eb37717bf4
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-07-27 12:22:42 -0400

    Create oddOrEven.py

---
+++ oddOrEven.py
@@ -0,0 +1,7 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+if number % 2 == 0 : 
+  print("Yeah, it's even!")
+else:
+  print("No, it's odd!")
