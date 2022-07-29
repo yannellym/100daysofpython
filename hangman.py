46532bc2263928c3fe0db30d04bc17ed197f9774
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-07-29 18:20:57 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,4 +1,63 @@
+# Detailed steps on how to build the hangman game!
+
 import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 #word list for game.
 word_list = ["cat", "pig", "horse", "rabbit", "dog", "goat", "ant", "rat", "mouse", "duck", "goose", "turkey", "chicken", "buffalo", "bird", "sheep", "monkey", "bear", "anaconda", "lion", "tiger", "slug", "cow", "squirell", "turtle", "dolphin", "elephant", "koala", "parrot", "frog", "bee", "emu", "zebra"]
@@ -8,20 +67,18 @@ word_list = ["cat", "pig", "horse", "rabbit", "dog", "goat", "ant", "rat", "mous
 # The user will choose a random letter that will be stored in a variable named "guess".
 # "Guess" will be compared against the chosen word and be verified to see if it is IN chosen_word.
 
-chosen_word = randon.choice(word_list)
- 
-guess = input("Guess a letter \n").lower()
-
-print(guess in chosen_word)
-  
+chosen_word = random.choice(word_list)
+print(chosen_word)
 # Step 2: Create a variable named display that will be a list. 
 # for every letter in the chosen_word add a "_" to the display.
 
 display = []
+game_over = False
+lives = 6
+letters_guessed = []
 
 for letters in chosen_word:
   display.append("_")
-
 
 # Step 3: Declare a variable named has_won. It will hold a value of false since the user hasn't won yet.
 # declare a while loop. It will have an input where the user will guess a letter.
@@ -31,17 +88,24 @@ for letters in chosen_word:
 # print the display
 # if there are no empty spaces left, the user has won. The has_won variable is now true. 
 
-has_won = False
-
-while not has_won:
+while not game_over:
   guess = input("Guess a letter: ").lower()
   dash = "_"
   #Check guessed letter
-  for position in range(word_length):
+  for position in range(len(chosen_word)):
     letter = chosen_word[position]
     if letter == guess:
         display[position] = letter
-        print(display)
-    if dash not in display:
-      print(f"You win! the word was {chosen_word}")
-      has_won = True
+        print(display) 
+  if dash not in display:
+    print(f"You win! the word was {chosen_word}")
+    game_over = True
+  
+  if guess not in chosen_word:
+    lives -= 1
+    letters_guessed.append(guess)
+    print(f"The letter is not in the word. You have {lives} lives left. Letters guessed : {letters_guessed}")
+    print(stages[lives])
+    if lives == 0:
+      print(f"You have {lives} lives left. You lose! :(")
+      game_over = True
