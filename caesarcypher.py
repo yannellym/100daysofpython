46affe75131ff9698b1c602eda8200c4a99045db
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-07-30 14:35:31 -0400

    Update caesarcypher.py

--- caesarcypher.py
+++ caesarcypher.py
@@ -1,18 +1,39 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""  
+
+print(logo)
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-  
 
 def caesar(text, shift, direction):
   cipher_text = ""
   if direction == "decode":
     shift *= -1 # makes this a negative number if user chooses decode
-  for letter in text:
-    position = alphabet.index(letter)
-    new_position = (position + shift) % 26 # the % is going to make sure it restarts a position 0 once it hits the 26th index.
-    cipher_text += alphabet[new_position]
+  for char in text:
+    if char in alphabet:
+      position = alphabet.index(letter)
+      new_position = (position + shift) % 26 # the % is going to make sure it restarts a position 0 once it hits the 26th index.
+      cipher_text += alphabet[new_position]
+    else:
+      cipher_text += char
   print(f"The {direction}d text is {cipher_text}")
   
 caesar(text, shift, direction)
