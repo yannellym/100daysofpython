0b1b20510258c0623d11fe7f41feb5b3f8f6f8f7
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-07-30 14:02:53 -0400

    Update caesarcypher.py

--- caesarcypher.py
+++ caesarcypher.py
@@ -3,27 +3,16 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-def encrypt(text, shift):
-  for i in text:
-    position = alphabet.index(i) 
-    new_position = position + shift
-    cypher_text = alphabet[new_position]
-    print(cypher_text)
   
 
-def decrypt(plain_text, shift_amount):
+def caesar(text, shift, direction):
   cipher_text = ""
-  for letter in plain_text:
+  for letter in text:
     position = alphabet.index(letter)
-    new_position = (position - shift_amount) % 26
-    print(new_position)
+    if direction == "decode"
+      shift *= -1
+    new_position = position + shift
     cipher_text += alphabet[new_position]
-  print(f"The decoded text is {cipher_text}")
-
-if direction == "encode":
-  encrypt(text, shift)
-elif direction == "decode":
-  decrypt(text, shift)
-else:
-  print("wrong spelling, please try again")
+  print(f"The {position}d text is {cipher_text}")
+  
+  caesar(text, shift, direction)
