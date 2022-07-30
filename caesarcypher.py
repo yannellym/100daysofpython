00410947263913a52d23a69582d3e681a0fd7f19
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-07-30 13:59:02 -0400

    Update caesarcypher.py

--- caesarcypher.py
+++ caesarcypher.py
@@ -11,4 +11,19 @@ def encrypt(text, shift):
     cypher_text = alphabet[new_position]
     print(cypher_text)
   
- encrypt("hello", 5)
+
+def decrypt(plain_text, shift_amount):
+  cipher_text = ""
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    new_position = (position - shift_amount) % 26
+    print(new_position)
+    cipher_text += alphabet[new_position]
+  print(f"The decoded text is {cipher_text}")
+
+if direction == "encode":
+  encrypt(text, shift)
+elif direction == "decode":
+  decrypt(text, shift)
+else:
+  print("wrong spelling, please try again")
