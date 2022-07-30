2e1c5e3343c79453d56fe1b0a7b21055a4110db7
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-07-30 12:46:57 -0400

    Create caesarcypher.py

---
+++ caesarcypher.py
@@ -0,0 +1,14 @@
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def encrypt(text, shift):
+  for i in text:
+    position = alphabet.index(i) 
+    new_position = position + shift
+    cypher_text = alphabet[new_position]
+    print(cypher_text)
+  
+ encrypt("hello", 5)
