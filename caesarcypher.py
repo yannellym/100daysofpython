bc5f2c8aea38b974955f6ae7d5e8d6c581eed001
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-07-30 14:13:05 -0400

    Update caesarcypher.py

--- caesarcypher.py
+++ caesarcypher.py
@@ -7,12 +7,12 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 def caesar(text, shift, direction):
   cipher_text = ""
+  if direction == "decode":
+    shift *= -1
   for letter in text:
     position = alphabet.index(letter)
-    if direction == "decode"
-      shift *= -1
     new_position = position + shift
     cipher_text += alphabet[new_position]
-  print(f"The {position}d text is {cipher_text}")
+  print(f"The {direction}d text is {cipher_text}")
   
-  caesar(text, shift, direction)
+caesar(text, shift, direction)
