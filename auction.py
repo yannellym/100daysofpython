6f9a86bcf06fa01c0c99ef92d313d45709d9c10a
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-07-31 16:26:31 -0400

    Create auction.py

---
+++ auction.py
@@ -0,0 +1,50 @@
+from turtle import clear
+
+logo = '''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+'''
+print(logo)
+print("Welcome to the secret auction program")
+
+bidders = {}
+
+active_auction = True
+highest_bidder = ""
+highest_bid = 0
+
+def input_bidder():
+    global active_auction
+    bidder_name = input("What is your name?  \n")
+    bidder_bid = int(input("What is your bid ? \n $"))
+
+    bidders[bidder_name] = {"bid": bidder_bid}
+    additional_bidder = input("Are there any other bidders ? Type Yes or No \n").lower()
+  
+    if additional_bidder == "yes":
+        input_bidder()
+    else:
+        active_auction = False
+
+def highest_bidder():
+    global highest_bidder
+    global highest_bid
+    for k, v in bidders.items():
+        if v["bid"] > highest_bid:
+            highest_bidder = k
+            highest_bid = v["bid"]
+    clear()
+    print(f"The highest bidder is {highest_bidder} with a bid of $ {highest_bid}.")
+
+while active_auction:
+    input_bidder()
+    highest_bidder()
