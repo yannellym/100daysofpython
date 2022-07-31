4fba869e46d87fc9621cc6e7459e5628dbd732b0
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-07-31 12:04:25 -0400

    Create transferdic.py

---
+++ transferdic.py
@@ -0,0 +1,19 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+#TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+#TODO-2: Write your code below to add the grades to student_grades.👇
+
+for k, v in student_scores.items():
+  student_grades[k] = v
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
