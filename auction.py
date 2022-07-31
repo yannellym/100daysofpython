bcb51e316a552eaad14f70e2c53e390d4534f97b
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-07-31 16:34:24 -0400

    Update auction.py

--- auction.py
+++ auction.py
@@ -27,7 +27,7 @@ def input_bidder():
     bidder_name = input("What is your name?  \n")
     bidder_bid = int(input("What is your bid ? \n $"))
 
-    bidders[bidder_name] = {"bid": bidder_bid}
+    bidders[bidder_name] = bidder_bid
     additional_bidder = input("Are there any other bidders ? Type Yes or No \n").lower()
   
     if additional_bidder == "yes":
@@ -39,12 +39,14 @@ def highest_bidder():
     global highest_bidder
     global highest_bid
     for k, v in bidders.items():
-        if v["bid"] > highest_bid:
+        if v> highest_bid:
             highest_bidder = k
-            highest_bid = v["bid"]
+            highest_bid = v
     clear()
     print(f"The highest bidder is {highest_bidder} with a bid of $ {highest_bid}.")
 
 while active_auction:
     input_bidder()
     highest_bidder()
+
+
