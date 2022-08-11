15e2791df8e4293a8c99d9d383c0c1d7c146558d
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-08-11 17:32:33 -0400

    Create higherorlower.py

---
+++ higherorlower.py
@@ -0,0 +1,40 @@
+from art import logo, vs 
+from game_data import data 
+import random
+#print(data)
+
+game_over = False
+cand_a_info = []
+cand_b_info = []
+higher_cand = None
+
+def cand_a():
+  global cand_a_info
+  random_number = random.randint(0,49)
+  cand_a_name = data[random_number]['name']
+  cand_a_des = data[random_number]['description']
+  cand_a_country = data[random_number]['country']
+  cand_a_count = data[random_number]['follower_count']
+  cand_a_info.extend([cand_a_name, cand_a_count])
+  
+  print(f"Compare A: {cand_a_name}, a {cand_a_des}, from {cand_a_country}. {cand_a_count}")
+ 
+
+def cand_b():
+  global cand_b_info
+  random_number = random.randint(0,49)
+  cand_b_name = data[random_number]['name']
+  cand_b_des = data[random_number]['description']
+  cand_b_country = data[random_number]['country']
+  cand_b_count = data[random_number]['follower_count']
+  cand_b_info.extend([cand_b_name, cand_b_count])
+  
+  print(f"Against B: {cand_b_name}, a {cand_b_des}, from {cand_b_country}.{cand_b_count}")
+
+def which_is_greater(cand_a_info, cand_b_info):
+  global higher_cand
+  if cand_a_info.pop() > cand_b_info.pop():
+    higher_cand = cand_a_info
+  else:
+    higher_cand = cand_b_info
+    
