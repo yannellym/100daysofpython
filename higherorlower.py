c65ac1f035a9c8588bdc6335924832f7763ff102
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-08-12 16:48:43 -0400

    Update higherorlower.py

--- higherorlower.py
+++ higherorlower.py
@@ -1,77 +1,61 @@
-# from art import logo, vs, lost
-from game_data import data 
+from game_data import data
 import random
-import replit 
+from art import logo, vs
+from replit import clear
 
-game_over = False
-cand_a_info = ["A"]
-cand_a_num = 0
-cand_b_info = ["B"]
-cand_b_num = 0
-higher_cand = [0]
-high_score = 0
+def get_random_account():
+  """Get data from random account"""
+  return random.choice(data)
 
-def cand_a():
-  global cand_a_info
-  global cand_a_num
-  cand_a_int = cand_a_num if higher_cand[0] == "A" else random.randint(0,49)
-  cand_a_num = cand_a_int
-  cand_a_name = data[cand_a_int]['name']
-  cand_a_des = data[cand_a_int]['description']
-  cand_a_country = data[cand_a_int]['country']
-  cand_a_count = data[cand_a_int]['follower_count']
-  cand_a_info.extend([cand_a_count])
-  print(f"higher cand: {higher_cand}")
-  print(f"Compare A: {cand_a_name}, a {cand_a_des}, from {cand_a_country}. {cand_a_count}")
- 
+def format_data(account):
+  """Format account into printable format: name, description and country"""
+  name = account["name"]
+  description = account["description"]
+  country = account["country"]
+  # print(f'{name}: {account["follower_count"]}')
+  return f"{name}, a {description}, from {country}"
 
-def cand_b():
-  global cand_b_info
-  global cand_b_num
-  cand_b_int = cand_b_num if higher_cand[0] == "B" else random.randint(0,49)
-  cand_b_num = cand_b_int
-  cand_b_name = data[cand_b_int]['name']
-  cand_b_des = data[cand_b_int]['description']
-  cand_b_country = data[cand_b_int]['country']
-  cand_b_count = data[cand_b_int]['follower_count']
-  cand_b_info.extend([cand_b_count])
-  print(f"higher cand: {higher_cand}")
-  print(f"Against B: {cand_b_name}, a {cand_b_des}, from {cand_b_country}.{cand_b_count}")
-
-def which_is_greater(cand_a_info, cand_b_info):
-  global higher_cand
-  if cand_a_info.pop() > cand_b_info.pop():
-    higher_cand = cand_a_info
+def check_answer(guess, a_followers, b_followers):
+  """Checks followers against user's guess 
+  and returns True if they got it right.
+  Or False if they got it wrong.""" 
+  if a_followers > b_followers:
+    return guess == "a"
   else:
-    higher_cand = cand_b_info
+    return guess == "b"
 
-def is_user_correct():
-  global high_score
-  global next_level
-  global guessed_correctly
-  
-  user_answer = input("Who has more followers? Type 'A' or 'B': ").upper()
 
-  if user_answer == higher_cand[0]:
-    high_score += 1
-    replit.clear()
-    print(f"✨✨ Correct ✨✨ Current score: {high_score}")
-    to_print()
-  else:
-    replit.clear()
-    next_level = False
-    print(f"Incorrect. Sorry you lost! Final score: {high_score}")
+def game():
+  print(logo)
+  score = 0
+  game_should_continue = True
+  account_a = get_random_account()
+  account_b = get_random_account()
 
-  
+  while game_should_continue:
+    account_a = account_b
+    account_b = get_random_account()
 
-def to_print():
-  #print(logo)
-  #print(cand_a_info, cand_b_info)
-  cand_a()
-  cand_b()    
-  which_is_greater(cand_a_info, cand_b_info)
-  is_user_correct()
-  
- 
-to_print()
+    while account_a == account_b:
+      account_b = get_random_account()
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}.")
+    
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    clear()
+    print(logo)
+    if is_correct:
+      score += 1
+      print(f"You're right! Current score: {score}.")
+    else:
+      game_should_continue = False
+      print(f"Sorry, that's wrong. Final score: {score}")
+
+game()
 
