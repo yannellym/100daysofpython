ffab98ad1e37452a254b7503f261e694a690a523
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-08-13 14:40:56 -0400

    Update coffeemachine.py

--- coffeemachine.py
+++ coffeemachine.py
@@ -5,20 +5,20 @@ def clear():
     print('\n' * 10)
 
 
-def get_change(user_money, item):
-    user_change = round(user_money - MENU[item]['cost'],2)
+def get_coffee(user_money, menu_item, user_input):
+    user_change = round(user_money - menu_item['cost'], 2)
     if user_change < 0:
         print("Sorry! You don't have enough money. Please accept your refund.")
-        get_my_coffee()
+        order_coffee()
     else:
-        print(f"Here is ${user_change} in change.")
-        print(f"Here is your {item} ☕️. Enjoy!")
-        return user_change
+        print(f"${user_change} is your change.")
+        print(f"Here is your {user_input} ☕️. Enjoy!")
+        return
 
 
-def get_my_coffee():
+def order_coffee():
     user_input = input("What would you like? espresso, latte, or cappuccino?: ").lower()
-
+    menu_item = MENU[user_input]
     if user_input == 'report':
         for i in resources:
             print(i, ":", resources[i])
@@ -26,21 +26,24 @@ def get_my_coffee():
         print("Powering Off. Goodbye!")
     else:
         clear()
-        print("Please insert coins.")
-        user_quarters = int(input("How many quarters?: ")) * .25
-        user_dimes = int(input("How many dimes?: ")) * .10
-        user_nickles = int(input("How many nickles?: ")) * .05
-        user_pennies = int(input("How many pennies?: ")) * .01
-        user_money = sum([user_pennies, user_nickles, user_dimes, user_quarters])
-        clear()
-        print(f"You gave me ${user_money} in total. ")
+        for i in menu_item['ingredients']:
+            for j in resources:
+                if resources[j] < menu_item['ingredients'][i]:
+                    print("not enough")
+                else:
+                    clear()
+                    print(f"The {user_input} costs ${menu_item['cost']} ")
+                    print("Please insert coins.")
+                    user_quarters = int(input("How many quarters?: ")) * .25
+                    user_dimes = int(input("How many dimes?: ")) * .10
+                    user_nickles = int(input("How many nickles?: ")) * .05
+                    user_pennies = int(input("How many pennies?: ")) * .01
+                    user_money = sum([user_pennies, user_nickles, user_dimes, user_quarters])
 
-        if user_input == 'espresso':
-            user_change = get_change(user_money, 'espresso')
-        elif user_input == 'latte':
-            user_change = get_change(user_money, 'latte')
-        else:
-            user_change = get_change(user_money, 'cappuccino')
+                    clear()
+                    print(f"You gave me ${user_money} in total. ")
+                    return get_coffee(user_money, menu_item, user_input)
 
 
-get_my_coffee()
+order_coffee()
+
