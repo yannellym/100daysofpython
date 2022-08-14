529cdf663fb9028288e26e0b71398ddf97dc5e1d
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-08-14 11:53:15 -0400

    Update coffeemachine.py

--- coffeemachine.py
+++ coffeemachine.py
@@ -5,7 +5,7 @@ def clear():
     print('\n' * 5)
 
 
-def use_resources(user_money, menu_item):
+def use_resources(menu_item):
     for i in resources:
         if i in menu_item['ingredients']:
             resources[i] = resources[i] - menu_item['ingredients'][i]
@@ -13,14 +13,14 @@ def use_resources(user_money, menu_item):
     print(resources)
 
 
-def get_coffee(user_money, menu_item, user_input):
+def get_coffee(total, menu_item, user_input):
     global order_again
-    user_change = round(user_money - menu_item['cost'], 2)
+    user_change = round(total - menu_item['cost'], 2)
     if user_change < 0:
         print("Sorry! You don't have enough money. Please accept your refund.")
-        print(f"Total refund: ${user_money}")
+        print(f"Total refund: ${total}")
     else:
-        use_resources(user_money, menu_item)
+        use_resources(total, menu_item)
         print(f"${user_change} is your change.")
         print(f"Here is your {user_input} ☕️. Enjoy!")
 
@@ -32,6 +32,44 @@ def get_coffee(user_money, menu_item, user_input):
         clear()
         order_again = False
         print("Thank you for ordering, Goodbye! ")
+
+
+def order_coffee():
+    user_input = input("What would you like? espresso, latte, or cappuccino?: ").lower()
+
+    if user_input == 'report':
+        for i in resources:
+            print(i, ":", resources[i])
+    elif user_input == "off":
+        print("Powering Off. Goodbye!")
+    else:
+        clear()
+        menu_item = MENU[user_input]
+        for i in menu_item['ingredients']:
+            for j in resources:
+                if resources[j] < menu_item['ingredients'][i]:
+                    print(f"Sorry! We do not have enough {j} for your order")
+                    print("Please order another item.")
+                    order_coffee()
+                else:
+                    clear()
+                    print(f"The {user_input} costs ${menu_item['cost']} ")
+                    print("Please insert coins.")
+                    total = int(input("How many quarters?: ")) * 0.25
+                    total += int(input("How many dimes?: ")) * 0.1
+                    total += int(input("How many nickles?: ")) * 0.05
+                    total += int(input("How many pennies?: ")) * 0.01
+
+                    clear()
+                    print(f"You gave me ${total} in total. ")
+                    return get_coffee(total, menu_item, user_input)
+
+
+order_again = True
+
+while order_again:
+    order_coffee()
+
 
 
 def order_coffee():
